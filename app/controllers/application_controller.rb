70ad235c5d4e72d868b6c16d7ec5cfde8c934717
Przemek Markowski <p.markowski1980@gmail.com>
2017-04-03 23:19:48 +0700

    finished layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+	before_action :get_category_nav
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def get_category_nav
+  	@category_nav = Category.all
+  end
 end
